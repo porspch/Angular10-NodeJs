c5b8f6cab1074fb824ebf86db9a282cefdd98efb
Suppakrit.P <suppakrit.pra@gmail.com>
2020-08-24 00:08:18 +0700

    redirect api to index.html of angular

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static(path.join(__dirname,'dist/porspch')));
 
+/**
 app.get('/api/test',function (req, res) {
     res.end("Hi, api");
+})
+*/
+
+app.use('*',function (req, res) {
+    // res.end("Hi, api");
+    res.sendFile(path.join(path.join(__dirname,'index.html')));
 })
 
 const server = app.listen(8081,function () {
